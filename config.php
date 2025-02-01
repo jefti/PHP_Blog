ca27801068f020fc4cf5ea86dc831f8ec318381c
Jefti Meira <jeftimeira@gmail.com>
2025-01-31 22:34:28 -0300

    feat: initial files

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+//Arquivo de configuração do sistema
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+//Arquivo principal da página
+    echo'<h1>Arquivo index</h1>';
+    
+//    print 'texto usando print';
+
+?>
